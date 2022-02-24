e27602ad45b28e2fc7036578739559fadaba1c86
Andrew Trice <triceam@gmail.com>
2022-02-24 13:19:00 -0500

    initial commit based on hcbt template (#1)
    * initial commit based on hcbt template
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * fixed var typo
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * fixed var typo
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated var names to match env vars
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * another mismatched name between tempaltes
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * missing pass-through vars
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * renamed access key
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * default region
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated aws provider handling of region
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updating keys to match provider
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * fixed env vars messed up by global search/replace
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * removed dupe provider block
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * one more
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * Comments out destroy
    
    * Changes to oc apply so doesn't fail when resources exist
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>
    
    * added sleep to validation script
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * merged
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * fixed bad yaml indentation on verify.yaml
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * aws configure list for debugging
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * moved aws configure list for debugging
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * added access/secret keys to local exec env vars
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated yaml for portworx operator
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * debugging portworx operatoy yaml
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * enabled portworx essentials flag
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * added cleanup script
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * provision flag
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * non-prompting execution for cleanup
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated destroy
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated cleanup & validation
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * removed dupe dependency
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated destroy and validation logic
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * added label to storagecluster so wipe script can find it
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * removed unnecessary cleanup
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated cleanup
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated cleanup
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated cleanup script
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * modified cleanup and validation scripts
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * more updates to cleanup and validation scripts
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * fixed copy/paste error
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * moved cluster login outside of the portworx module
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * refactor portworx input var object
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * refactor portworx input var object
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * added default enable_encryption value
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * refactor, added readmes, updated module.yaml
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    Co-authored-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ test/stages/stage2-aws-portworx.tf
@@ -0,0 +1,21 @@
+
+locals {
+  portworx_config = {
+    enable = true
+    cluster_id = var.px_cluster_id
+    user_id = var.px_user_id
+    osb_endpoint = var.px_osb_endpoint
+    type = "essentials"
+    enable_encryption = false
+  }
+}
+
+module "aws_portworx" {
+  source = "./module"
+
+  region                = var.region
+  access_key            = var.access_key
+  secret_key            = var.secret_key
+  cluster_config_file   = module.dev_cluster.platform.kubeconfig
+  portworx_config       = local.portworx_config
+}
--- version.tf
+++ version.tf
@@ -1,3 +1,17 @@
 terraform {
   required_version = ">= 0.15.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
