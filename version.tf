d09429465ff45629b013f82057ad5a3811971d2b
Andrew Trice <amtrice@us.ibm.com>
2022-05-27 12:16:29 -0400

    readme cleanup and updated portworx version
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- version.tf
+++ version.tf
@@ -15,3 +15,31 @@ terraform {
     }
   }
 }
+
+
+
+az ad sp create-for-rbac --role=a3dcc999-0424-4c08-b563-3b7a7a9a8e91 --scopes="/subscriptions/bc1627c6-ec80-4da3-8d18-03e91330e2f1/resourcegroups/aro-y5sdwcze"
+
+az role definition create --role-definition '{
+    "Name": "portworx-cloud-drive-2",
+    "Description": "",
+    "AssignableScopes": [
+    "/subscriptions/bc1627c6-ec80-4da3-8d18-03e91330e2f1"
+  ],
+  "Permissions": [
+  {
+    "Actions": [
+      "Microsoft.ContainerService/managedClusters/agentPools/read",
+      "Microsoft.Compute/disks/delete",
+      "Microsoft.Compute/disks/write",
+      "Microsoft.Compute/disks/read",
+      "Microsoft.Compute/virtualMachines/write",
+      "Microsoft.Compute/virtualMachines/read",
+      "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/write",
+      "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/read"
+    ],
+    "NotActions": [],
+    "DataActions": [],
+    "NotDataActions": []
+  }]
+}'
