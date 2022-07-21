0d2e0d39ebc3895d6d109f004039e57a039ad595
indirakalagara <kindira@gmail.com>
2022-07-21 18:21:16 +0530

    updated validate script
    Signed-off-by: indirakalagara <kindira@gmail.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -9,22 +9,7 @@ export KUBECONFIG=$(cat .kubeconfig)
 echo "sleeping for 2 mins to prevent synchronization errors"
 sleep 2m
 
-oc rollout status deployment/portworx-operator -n kube-system
-
-PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o json | jq -r '.items[] | .metadata.name' | head -1)
-
-if [[ -z "${PX_POD}" ]]; then
-  echo "Portworx pod name not found" >&2
-  exit 1
-else
-  echo "Portworx pod name: ${PX_POD}"
-fi
-
-kubectl exec "${PX_POD}" -n kube-system -- /opt/pwx/bin/pxctl status
-
-exit 0
-
-# echo "checking for portworx services"
+echo "checking for portworx services"
 
 
 # count=0
@@ -42,3 +27,21 @@ exit 0
 # oc rollout status daemonset/portworx-api -n kube-system
 
 # exit 0
+
+===
+
+oc rollout status deployment/portworx-operator -n kube-system
+
+PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o json | jq -r '.items[] | .metadata.name' | head -1)
+
+if [[ -z "${PX_POD}" ]]; then
+  echo "Portworx pod name not found" >&2
+  exit 1
+else
+  echo "Portworx pod name: ${PX_POD}"
+fi
+
+#kubectl exec "${PX_POD}" -n kube-system -- /opt/pwx/bin/pxctl status
+
+exit 0
+=====
